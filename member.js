b04bd611092e27ddab3f6892b7f2cb014c9e8616
warhursttw10 <warhursttw10@gmail.com>
2024-01-19 16:01:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selected
+}
